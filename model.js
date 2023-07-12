d6ab8a6b45150598683b6ad320b535320dc8967c
Yanina Ayala <yaninacayala@gmail.com>
2023-07-12 19:35:32 -0300

    Ejercicio resuelto correctamente

--- model.js
+++ model.js
@@ -1,13 +1,19 @@
-import jsonfile from 'jsonfile';
-const PATH = './data.json';
+import jsonfile from "jsonfile";
+const PATH = "./data.json";
 
 const db = jsonfile.readFileSync(PATH);
 
 // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ COMPLETAR SOLO ESTA FUNCIÓN ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 function obtenerProvinciasSegunMeridiano(meridiano) {
-
-    // COMPLETAR
+  const provincias = db.provincias;
+  const resultado = provincias.filter((prov) => {
+    const longitud = Math.abs(Math.trunc(prov.centroide.lon));
+    if (longitud == meridiano) {
+      return prov;
+    }
+  });
+  return resultado;
 }
 
 // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ COMPLETAR SOLO ESTA FUNCIÓN ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
